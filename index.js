74f8cb51d8b0bd3561ecdad9d437ea145ade0136
serafimpargaru <serafimpargaru@gmail.com>
2024-01-14 20:13:10 +0100

    Update

---
+++ index.js
@@ -0,0 +1,43 @@
+const url = "https://striveschool-api.herokuapp.com/books";
+let arrayLibri = [];
+
+window.onload = getBooks = () => {
+  fetch(url, {})
+    .then((risposta) => risposta.json())
+    .then((resultLibri) => {
+      arrayLibri = resultLibri;
+      creaCard(arrayLibri); //richiama la funzione con l'array sopra creato
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+const creaCard = function (libri) {
+  const container = document.querySelector("#main_books");
+  libri.map((x) => {
+    container.innerHTML += `<div class="card p-0" style="width: 18rem">
+    <img src="${x.img}" class="card-img-top img-mod" alt="... " />
+    <div class="card-body d-flex flex-column justify-content-between">
+      <h5 class="card-title">${x.title}</h5>
+      <div class="d-flex justify-content-between align-items-center">
+        <p class="card-text text-success fw-bold fs-4 m-0">€${x.price}</p>
+        <div>
+          <button class="btn bg-success-subtle btn-sm" id="add_btn">Aggiungi</button>
+          <button class="btn bg-success-subtle btn-sm" id="del_btn">Rimuovi</button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  });
+};
+
+//aggiunta dell'item al carrello
+let cartArray = [];
+
+const addButton = document.querySelector("#add_btn");
+addButton.addEventListener("click", addToCart);
+
+function addToCart(arrayCarrello, addButton) {
+
+}
